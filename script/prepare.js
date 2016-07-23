167ccd52e89b4e7802ee91d83d10abe7e2238559
Jiang Yan <572058317@qq.com>
2016-07-24 07:36:09 +0800

    some changes

--- script/prepare.js
+++ script/prepare.js
@@ -1,39 +1,67 @@
 
+/*                            _ooOoo_  
+ *                           o8888888o  
+ *                           88" . "88  
+ *                           (| -_- |)  
+ *                            O\ = /O  
+ *                        ____/`---'\____  
+ *                      .   ' \\| |// `.  
+ *                       / \\||| : |||// \  
+ *                     / _||||| -:- |||||- \  
+ *                       | | \\\ - /// | |  
+ *                     | \_| ''\---/'' | |  
+ *                      \ .-\__ `-` ___/-. /  
+ *                   ___`. .' /--.--\ `. . __  
+ *                ."" '< `.___\_<|>_/___.' >'"".  
+ *               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+ *                 \ \ `-. \_ __\ /__ _/ .-` / /  
+ *         ======`-.____`-.___\_____/___.-`____.-'======  
+ *                            `=---='  
+ *  
+ *         .............................................   */
+
 var i,j;
 var boxWidth  = 25;
 var boxHeight = 17;
-var snake     = declarationSnake();
+var direct    = "right";
+var snake     = new Array();
+var snakeBody = new Array();
 
+addLoadEvent(initSnake);
+addLoadEvent(initSnakeBody);
 addLoadEvent(start);
 addLoadEvent(restart);
-addLoadEvent(test);
 
-function declarationSnake() {
-	var snake = new Array();
+function initSnake() {
 	for(i = 0; i < boxHeight; i++ ) {
 		snake[i] = new Array();
 		for(j = 0; j < boxWidth; j++ ) {
 			snake[i][j] = false;
 		}
 	}
-	return snake;
+}
+
+function initSnakeBody() {
+	for(i = 0; i < 3; i++ ) {
+		snakeBody[i] = new Array();
+		snakeBody[i].x = 2;
+		snakeBody[i].y = i;
+	}
 }
 
 function start() {
-	for(i = 0; i < boxHeight; i++) {
-		for(j = 0; j < boxWidth; j++) {
+	for(i in snake) {
+		for(j in snake[i]) {
 			snake[i][j] = false;
 		}
 	}
-	for(j = 0; j < 3; j++ ) {
-		snake[2][j] = true;
+	for(i in snakeBody) {
+		snake[snakeBody[i].x][snakeBody[i].y] = true;
 	}
 	rendering(snake);
-	var button = document.getElementsByTagName("table")[0];
-	button.focus();
 }
 
-function rendering(snake) {
+function rendering() {
 	var td = document.getElementsByTagName("td");
 	for(var k=0,i = 0; i < boxHeight; i++) {
 		for(j = 0; j < boxWidth; j++,k++) {
@@ -54,16 +82,31 @@ function restart() {
 }
 
 function test() {
-	for(j = 0; j <= boxWidth ; j++) {
+	//alert(snake[2]);
+	rendering(snake);
+	for(j = 0; j < boxWidth ; j++) {
 		if(snake[2][j] === true) {
-			if(snake[2][boxWidth] === true ) {
-				snake[2][0] = true;
-			}
 			snake[2][++j] = true;
-			if(j-3 >= 0)
-				snake[2][j-3] = false;
+			snake[2][j-3] = false;
 		}
 	}
-	rendering(snake);
+	if(snake[2].indexOf(true)>=23)
+		test2();
+		return;
 	setTimeout("test()",100);
+}
+
+function test2() {
+	rendering();
+	for(j = 0; j < boxWidth ; j++) {
+		if(snake[2][j] === true) {
+			snake[2][j-1] = true;
+			snake[2][j+3] = false;
+		}
+	}
+	if(snake[2].indexOf(true) === 0) {
+		test();
+		return;
+	}
+	setTimeout("test2()",100);
 }
