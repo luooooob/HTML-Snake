fe6973b13bda764a0ce9af624437b1bfa6816190
Jiang Yan <572058317@qq.com>
2016-07-25 21:05:54 +0800

    666

--- prepare.js
+++ prepare.js
@@ -1,4 +1,10 @@
-
+/* ==============================================================
+ * 
+ * 
+ * ============================================================== 
+ * 
+ * 
+ * ============================================================== */
 var boxWidth  = 25;
 var boxHeight = 17;
 var snakeBox  = new Array();
@@ -6,16 +12,18 @@ var snake     = new Array();
 var food      = new Object();
 var instruct  = "right";
 var gameOver  = false;
+var run;
 
 
-addLoadEvent(start);
-addLoadEvent(restart);
+addLoadEvent(prepareStart);
 addLoadEvent(keyBoardInstruct);
+addLoadEvent(restart);
 
-function start() {
+
+function prepareStart() {
+	clearInterval(run);
 	init_snakeBox();
-	init_snake();
-	set_food();
+	set_food(); 
 	rendering();
 }
 
@@ -25,13 +33,10 @@ function init_snakeBox() {
 		for(j = 0; j < boxWidth; j++ ) {
 			snakeBox[i][j] = false;
 		}
-	}
-}
-
-function init_snake() {
+	}//init_snakeBox
 	for(i = 0; i < 3; i++ ) {
 		snake[i] = {x: 2, y: i};
-	}
+	}//init_snake
 }
 
 function set_food() {
@@ -68,37 +73,46 @@ function rendering() {
 function restart() {
 	var button = document.getElementsByTagName("button")[0];
 	button.onclick = function(event) {
-		start();
+		clearInterval(run);
+		prepareStart();
 		event.preventDefault();
 	}
 }
 
-function keyBoardInstruct () {
+function keyBoardInstruct() {
 	document.onkeydown = function(event) {
+		var instruct_direction = true;
 		switch(event.keyCode) {
-			event.preventDefault();
+			case 32: instruct = "space"; break;
 			case 37: instruct = "left"; break;
 			case 38: instruct = "top"; break;
 			case 39: instruct = "right"; break;
 			case 40: instruct = "down"; break;
+			default: 
+				init_snakeBox();
+				rendering();		
+				go_go_go();
 		}
+		event.preventDefault();
 	}
 }
 
-function playing() {
-	document.onkeydown
+function go_go_go() {
+	run = setInterval("running()",100);
 
 }
 
 function running() {
 	var len = snake.length;
-	var head = {x: snake[len-1].x, y: snake[len-1].y};
+	var head = new Object();
+	head.x = snake[len-1].x;
+	head.y = snake[len-1].y;
 	switch(instruct) {
 		case "top": 
 			if(head.x-1 >= 0)
 				snake[len] = {x: head.x-1, y: head.y};
 			break;
-		case "bottom": 
+		case "down": 
 			if(head.x+1 < boxHeight)
 				snake[len] = {x: head.x+1, y: head.y};
 			break;
@@ -106,7 +120,7 @@ function running() {
 			if(head.y-1 >= 0)
 				snake[len] = {x: head.x, y: head.y-1};
 			break;
-		case "down": 
+		case "right": 
 			if(head.y+1 < boxWidth)
 				snake[len] = {x: head.x, y: head.y+1};
 			break;
@@ -114,5 +128,45 @@ function running() {
 	for(i = 0; i <snake.length-1; i++) {
 		snake[i] = snake[i+1];
 	}
-	snake.pop();
+	if(snake[snake.length-1].x === food.x &&  snake[snake.length-1].y === food.y) {
+		set_food();
+	}
+	else
+		snake.pop();
+	rendering();
+}
+
+
+/* ==============================================================
+ * 
+ * 
+ * ============================================================== 
+ * 
+ * 
+ * ============================================================== */
+
+function hasClass(obj, cls) {
+ 	return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+function addClass(obj, cls) {  
+    if (!this.hasClass(obj, cls)) obj.className += " " + cls;  
+}  
+  
+function removeClass(obj, cls) {  
+    if (hasClass(obj, cls)) {  
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');  
+        obj.className = obj.className.replace(reg, ' ');  
+    }  
+}
+function addLoadEvent(func) { 
+	var oldonload = window.onload; 
+	if (typeof window.onload != 'function') { 
+		window.onload = func; 
+	} 
+	else { 
+		window.onload = function() { 
+			oldonload(); 
+			func(); 
+		} 
+	} 
 }
