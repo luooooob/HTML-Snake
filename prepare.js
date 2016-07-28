f0d6cd7c91540b8a0eba6d594e86b39aff080be2
Jiang Yan <572058317@qq.com>
2016-07-28 17:38:29 +0800

    finished

--- prepare.js
+++ prepare.js
@@ -44,15 +44,15 @@ var food      = new food();
 var instruct  = new instruct();
 var gameOver  = false;
 var score     = 0;
-var runStates = false;
+var runStates  = false;
+var runSpeed  = 0;
 var run;
 
 function snakeBox() {
-	for(i = 0; i < boxHeight; i++ ) {
-		this[i] = new Array();
-	}
+/*	snakeBox类, boolean型二维数组储存坐标*/
 	this.init = function() {
 		for(i = 0; i < boxHeight; i++ ) {
+			this[i] = new Array();
 			for(j = 0; j < boxWidth; j++ ) {
 				this[i][j] = false;
 			}
@@ -62,9 +62,7 @@ function snakeBox() {
 }
 
 function snake() {
-	for(i = 0; i < 3; i++ ) {
-		this[i] = new Object();
-	}
+/*	snake类, 蛇坐标值数组*/
 	this.init = function() {
 		for(i = 0; i < 3; i++ ) {
 			this[i] = {x: 2, y: i};
@@ -75,8 +73,7 @@ function snake() {
 }
     
 function food() {
-	this.x = new Object();
-	this.y = new Object();
+/*	食物类, set方法返回随机非蛇区域坐标*/
 	this.set = function() {
 		this.x = Math.floor(Math.random()*boxHeight);
 		this.y = Math.floor(Math.random()*boxWidth);
@@ -89,7 +86,7 @@ function food() {
 }
 
 function instruct() {
-	this.value = "right";
+/*	键盘指令类*/
 	this.init = function() {
 		this.value = "right";
 	}
@@ -101,16 +98,37 @@ function instruct() {
 
 addLoadEvent(prepareStart);
 addLoadEvent(keyBoardInstruct);
-addLoadEvent(restart);
+addLoadEvent(restartEvent);
 
 function prepareStart() {
+/*	初始化*/
 	snakeBox.init();
 	snake.init();
 	instruct.init()
 	runStates = false;
-	score = 0;
+	runSpeed  = 0;
+	score     = 0;
 	food.set();
 	rendering();
+}
+
+function restartEvent() {
+/*	定义重新开始按钮事件*/
+	var button = document.getElementsByTagName("button")[0];
+	button.onclick = function(event) {
+		if(runStates)
+			runStates = false;
+		else {
+			/*清除game over, 暂停*/
+			var para1 = document.querySelector(".game-over");
+			var para2 = document.querySelector(".game-stop");
+			removeClass(para1, "active");
+			removeClass(para2, "active");
+			gameOver = false;
+		}
+		prepareStart();
+		event.preventDefault();
+	}
 }
 
 function rendering() {
@@ -126,49 +144,95 @@ function rendering() {
 	var td = document.getElementsByTagName("td");
 	for(var k=0,i = 0; i < boxHeight; i++) {
 		for(j = 0; j < boxWidth; j++,k++) {
+			removeClass(td[k],'food');
 			if(snakeBox[i][j] === true)
 				addClass(td[k],'active');
 			else
 				removeClass(td[k],'active');
+			if(i === food.x && j === food.y)
+				addClass(td[k],'food');
 		}
 	}
 	var scorePara = document.querySelector("#score");
 	scorePara.innerHTML = "score: <span>"+score+"</span>";
 }
 
-function restart() {
-	var button = document.getElementsByTagName("button")[0];
-	button.onclick = function(event) {
-		if(runStates)
-			clearInterval(run);
-		prepareStart();
+function gameStopEvent() {
+	var para = document.querySelector(".game-stop");
+	if(!runStates) {
+		removeClass(para, "active");
+		runStates = true;
+		go_go_go();
+	}
+	else {
+		runStates = false;
+		addClass(para, "active");
+	}
+}
+
+function gameOverEvent() {
+	runStates = false;
+	var para = document.querySelector(".game-over");
+	addClass(para, "active");
+}
+
+
+/* ==============================================================
+ * use key board to eat everything!
+ * ============================================================== */
+
+
+function keyBoardInstruct() {
+/*	keycode 32: space;
+
+	keycode 37: left
+	keycode 38: top
+	keycode 39: right
+	keycode 40: down
+
+	keycode 65: a;
+	keycode 87: w;
+	keycode 68: d;
+	keycode 83: s;*/
+	document.onkeydown = function(event) {
+		if(!runStates && runSpeed === 0) {
+			runStates = true;
+			go_go_go();
+		}
+		else switch(event.keyCode) {
+			case 32: 
+				gameStopEvent();
+			case 65:
+			case 37:
+				if(instruct.value === "right") break; 
+				instruct.value = "left"; break;
+			case 87:
+			case 38: 
+				if(instruct.value === "down") break;
+				instruct.value = "top"; break;
+			case 68:
+			case 39: 
+				if(instruct.value === "left") break;
+				instruct.value = "right"; break;
+			case 83:
+			case 40: 
+				if(instruct.value === "top") break;
+				instruct.value = "down"; break;
+
+		}
 		event.preventDefault();
 	}
 }
 
-function keyBoardInstruct() {
-	document.onkeydown = function(event) {
-		if(!runStates)
-			gameStop();
-		else switch(event.keyCode) {
-			case 32: 
-				gameStop();
-				event.preventDefault();
-				break;
-			case 37: instruct.value = "left"; break;
-			case 38: instruct.value = "top"; break;
-			case 39: instruct.value = "right"; break;
-			case 40: instruct.value = "down"; break;
-		}
-	}
-}
-
 function go_go_go() {
-	runStates = true;
-	run = setInterval("running()",200);
+	runSpeed  = Math.max(250 - snake.len*3, 90);
+	run = setTimeout("running()",runSpeed);
+
 }
 
 function running() {
+	if(!runStates) 
+		return;
 	var head = new Object();
 	var len = snake.len;
 	head.x = snake[len-1].x;
@@ -177,50 +241,42 @@ function running() {
 		case "top": 
 			if(head.x-1 >= 0)
 				snake[len] = {x: head.x-1, y: head.y};
+			else gameOver = true ;
 			break;
 		case "down": 
 			if(head.x+1 < boxHeight)
 				snake[len] = {x: head.x+1, y: head.y};
+			else gameOver = true;
 			break;
 		case "left":
 			if(head.y-1 >= 0)
 				snake[len] = {x: head.x, y: head.y-1};
+			else gameOver = true;
 			break;
 		case "right":
-			if(head.y+1 < boxWidth)
+			if(head.y+1 < boxWidth)     
 				snake[len] = {x: head.x, y: head.y+1};
+			else gameOver = true;
 			break;
-		default:
-			gameOver();
 	}
-	if(snake[snake.len].x === food.x &&  snake[snake.len].y === food.y) {
+	for(i = 0; i < snake.len; i++) {
+		if(snake[len].x === snake[i].x && snake[len].y === snake[i].y)
+			gameOver = true;
+	}
+	if(gameOver) {
+		gameOverEvent();
+		return;
+	}
+	if(snake[snake.len].x === food.x && snake[snake.len].y === food.y) {
 		food.set();
 		snake.len++;
 		score++;
-		running();
-		return;
 	}
 	else for(i = 0; i < snake.len; i++) {
 			snake[i]=snake[i+1];
 	}
 	rendering();
+	go_go_go();
 }
 
-function gameStop() {
-	var para = document.querySelector(".game-stop");
-	if(!runStates) {
-		removeClass(para, "active");
-		go_go_go();
-		alert("go_go_go");
-	}
-	else {
-		clearInterval(run);
-		runStates = false;
-		addClass(para, "active");
-	}
-}
-
-function gameOver() {
-	;
-}
 
