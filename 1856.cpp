90fa56a86829a9d9923917d439878f2297e6ba73
Jiang Yan <572058317@qq.com>
2016-07-29 22:21:04 +0800

    test

--- 1856.cpp
+++ 1856.cpp
@@ -1,25 +1,23 @@
 #include<stdio.h>
 #define N 10000000
 int father[N],num[N];
-void initial()
+void initial()/*初始化*/
 {
     int i;
     for(i=1;i<=N;i++)
     {
         father[i]=i;
-        num[i]=1;
+        num[i]=1;/*开始时数量都为1，根节点为自己*/
     }
 }
-
-int find(int x)
+int find(int x) /*寻找根节点*/
 {
     if(father[x]!=x)
         father[x]=find(father[x]);
     return father[x];
 }
-void merge(int a,int b)
+void merge(int a,int b)/*合并a和b*/
 {
-
     int p=find(a);
     int q=find(b);
     if(p!=q)
@@ -27,7 +25,6 @@ void merge(int a,int b)
         father[p]=q;
         num[q]+=num[p];
     }
-   // return h[q];
 }
 int main()
 {
@@ -46,7 +43,7 @@ int main()
             scanf("%d%d",&a,&b);
             if(a>max)
                 max=a;
-            if(b>max);
+            if(b>max)
                 max=b;
             merge(a,b);
         }
